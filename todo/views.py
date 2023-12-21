880e79f51311bb61edaba0ca032513df7b266d80
Rodrigo Santos <couriersix@Air-de-Rodrigo.fibertel.com.ar>
2023-12-21 19:22:35 -0300

    Filter improved, now filters by content, creation and tag

--- todo/views.py
+++ todo/views.py
@@ -2,32 +2,22 @@ from rest_framework import viewsets, permissions
 from .models import Todo, Tag
 from .serializers import TodoSerializer, TagSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+
 from django_filters import rest_framework as filters
 
 
 class TodoFilter(filters.FilterSet):
-    tags__name = filters.ChoiceFilter(choices=[])
+    created_at = filters.DateFromToRangeFilter()
+    description = filters.CharFilter(lookup_expr="icontains")
 
     class Meta:
         model = Todo
-        fields = ["tags__name"]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        user = self.request.query_params.get("user")
-        if user:
-            self.filters["tags__name"].extra.update(
-                {
-                    "choices": [
-                        (tag.name, tag.name) for tag in Tag.objects.filter(user=user)
-                    ]
-                }
-            )
+        fields = ["created_at", "description", "tags__name"]
 
 
 class TodoViewSet(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [filters.DjangoFilterBackend]
     filterset_class = TodoFilter
 
     def get_queryset(self):
@@ -41,6 +31,3 @@ class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)

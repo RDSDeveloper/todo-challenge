33efe90eca7bbf5c3ebb63b42ab95514ad901894
Rodrigo Santos <couriersix@Air-de-Rodrigo.fibertel.com.ar>
2023-12-21 15:03:05 -0300

    Model todo testing created with the serializer

--- todo/tests.py
+++ todo/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from .models import Todo, Tag
+from .serializers import TodoSerializer, TagSerializer
+from django.utils import timezone
 
 
 class TodoModelTest(TestCase):
@@ -28,3 +30,35 @@ class TodoModelTest(TestCase):
         tag = Tag.objects.get(id=1)
         expected_object_name = f"{tag.name}"
         self.assertEquals(expected_object_name, "tag1")
+
+    from django.utils import timezone
+
+    def test_todo_serializer(self):
+        todo = Todo.objects.get(id=1)
+        serializer = TodoSerializer(todo)
+        self.assertEqual(
+            serializer.data,
+            {
+                "id": 1,
+                "title": "Test Todo",
+                "description": "",
+                "completed": False,
+                "created_at": todo.created_at.isoformat().replace("+00:00", "Z"),
+                "updated_at": todo.updated_at.isoformat().replace("+00:00", "Z"),
+                "due_date": None,
+                "priority": "M",
+                "tags": [],
+                "user": 1,
+            },
+        )
+
+    def test_tag_serializer(self):
+        tag = Tag.objects.get(id=1)
+        serializer = TagSerializer(tag)
+        self.assertEqual(
+            serializer.data,
+            {
+                "id": 1,
+                "name": "tag1",
+            },
+        )
--- todo/urls.py
+++ todo/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from .views import TodoViewSet, TagViewSet
 
 router = routers.DefaultRouter()
-router.register(r"todos", TodoViewSet)
-router.register(r"tags", TagViewSet)
+router.register(r"todos", TodoViewSet, basename="todo")
+router.register(r"tags", TagViewSet, basename="tag")
 
 urlpatterns = [
     path("", include(router.urls)),

e1b7628a3585b54f368a4a17219c675510764c75
Rodrigo Santos <couriersix@Air-de-Rodrigo.fibertel.com.ar>
2023-12-21 20:00:09 -0300

    Integration testing finished

--- todo/tests.py
+++ todo/tests.py
@@ -1,8 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from .models import Todo, Tag
 from .serializers import TodoSerializer, TagSerializer
-from django.utils import timezone
+from django.urls import reverse
 
 
 class TodoModelTest(TestCase):
@@ -63,3 +63,80 @@ class TodoModelTest(TestCase):
                 "user": "testuser",
             },
         )
+
+
+class TodoIntegrationTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser", password="testpass"
+        )
+        cls.todo = Todo.objects.create(title="Test Todo", user=cls.user)
+        tag1 = Tag.objects.create(name="tag1", user=cls.user)
+        cls.todo.tags.add(tag1)
+
+    def setUp(self):
+        self.client = Client()
+        self.client.login(username="testuser", password="testpass")
+
+    def test_get_todos(self):
+        response = self.client.get(reverse("todo-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()[0]["title"], "Test Todo")
+
+    def test_create_todo(self):
+        response = self.client.post(reverse("todo-list"), {"title": "New Todo"})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Todo.objects.count(), 2)
+        self.assertEqual(Todo.objects.latest("created_at").title, "New Todo")
+
+
+class TagIntegrationTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser", password="testpass"
+        )
+
+    def setUp(self):
+        self.client = Client()
+        self.client.login(username="testuser", password="testpass")
+
+    def test_get_tags(self):
+        Tag.objects.create(name="Existing Tag", user=self.user)
+        response = self.client.get(reverse("tag-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()[0]["name"], "Existing Tag")
+
+    def test_create_tag(self):
+        response = self.client.post(reverse("tag-list"), {"name": "New Tag"})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Tag.objects.count(), 1)
+        self.assertEqual(Tag.objects.latest("id").name, "New Tag")
+
+
+class TodoFilterIntegrationTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser", password="testpass"
+        )
+
+    def setUp(self):
+        self.client = Client()
+        self.client.login(username="testuser", password="testpass")
+        Todo.objects.all().delete()
+
+    def test_filter_todos_by_description(self):
+        Todo.objects.create(
+            title="Test Todo 1", description="Description 1", user=self.user
+        )
+        Todo.objects.create(
+            title="Test Todo 2", description="Description 2", user=self.user
+        )
+        response = self.client.get(
+            reverse("todo-list"), {"description": "Description 1"}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 1)
+        self.assertEqual(response.json()[0]["title"], "Test Todo 1")
